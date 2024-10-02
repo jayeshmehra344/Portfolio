51d46abb29c14f86a92204d20fb36538f166e7d4
jayeshmehra344 <jayeshmehra344@gmail.com>
2024-10-03 01:59:18 +0530

    comm

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import React from 'react';
+import Home from "./components/home/Home.js"
 import './App.module.scss';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+
 import BaseLayout from "./components/BaseLayout";
 import {BrowserRouter} from "react-router-dom";
 
 function App() {
    return (
       <div>
+          <Router>
+      <Routes>
+        {/* Default route */}
+        <Route exact path="/" component={Home} /> 
+        </Routes>
+        </Router>
          <BrowserRouter>
             <BaseLayout/>
          </BrowserRouter>
